f06e120f7868443042203869c5bd099f82b6fd11
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-26 17:10:54 +0100

    Work in progress ..

--- src/js224eh_assign1/intCollection/ArrayIntList.java
+++ src/js224eh_assign1/intCollection/ArrayIntList.java
@@ -9,17 +9,51 @@ package js224eh_assign1.intCollection;
 
 public class ArrayIntList extends AbstractIntCollection implements IntList
 {
+    /**
+     * Add integer n to the end of the list.
+     *
+     * @param n The integer to add.
+     */
     @Override
     public void add(int n)
     {
-        /* TODO: implement .. */
+        if (values.length < size()) {
+            resize();
+        }
+
+        values[size++] = n;
     }
 
+    /**
+     * Inserts integer n at position index.
+     * Shifts the element currently at that position (if any) and any
+     * subsequent elements to the right.
+     *
+     * @param n     The integer to insert.
+     * @param index The position in which to insert integer n.
+     * @throws IndexOutOfBoundsException
+     */
     @Override
     public void addAt(int n, int index)
             throws IndexOutOfBoundsException
     {
-        /* TODO: implement .. */
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (values.length == size()) {
+            resize();
+        }
+
+        /* Make space for the new element by shifting all elements to the
+           right of the inserted elements one position over to the right. */
+        for (int i = size; i > index; i--) {
+            values[i] = values[i - 1];
+        }
+
+        /* Insert the new element at the now empty position. */
+        values[index] = n;
+        size++;
     }
 
     @Override
