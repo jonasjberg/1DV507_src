3733eaa2ef8f68b1895935e29535826a29b22cef
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-28 23:26:47 +0100

    Add skeleton #4.

---
+++ src/js224eh_assign1/SumMain.java
@@ -0,0 +1,12 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 1: Inheritance, Recursion and Java 1.8
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-01-28.
+
+package js224eh_assign1;
+
+
+public class SumMain
+{
+}
