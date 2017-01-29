a096b54fc4886c298be477efa8ad266c5b60cdce
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-29 01:25:44 +0100

    Works but uses different algorithm.

--- src/js224eh_assign1/SumMain.java
+++ src/js224eh_assign1/SumMain.java
@@ -23,6 +23,18 @@ public class SumMain
 {
     public static void main(String[] args)
     {
+        int n = 3;
+
+        int m = n / 2;
+        int o = n / 2 + 1;
+
+        System.out.printf("n: %d  n/2: %d  n/2+1: %d%n", n, m, o);
+
+        System.out.println(recursiveSum(0, 1));
+        System.out.println(recursiveSum(0, 2));
+        System.out.println(recursiveSum(0, 3));
+        System.out.println(recursiveSum(0, 4));
+        System.out.println(recursiveSum(0, 5));
 
     }
 
@@ -30,26 +42,55 @@ public class SumMain
      *      sum [1..N]  =  sum [1 .. N/2]  +  sum [N/2+1 .. N]
      *
      *
+     *      Full range, 1 to N:     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+     *                              |                           |
+     *                              1 ......................... N
      *
-     *      Full range:     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+     *      Halved, with N = 6:     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+     *                              |              |  |         |
+     *                              1 .......... N/2  N/2+1 ... N
      *
-     *      with N = 6:     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-     *                      1 .............N
-     *
-     *      gives:
+     *      Gives:
      *          sum [1..6]  =  sum [1 .. 6/2]  +  sum [6/2+1 .. 6]
      *                      =  sum [1 .. 3]    +  sum [4 .. 6]
      *
+     *      Recursion breaks down the terms:
+     *          First term:
+     *              sum [1..3]  =  sum [1 .. 3/2]  +  sum [3/2+1 .. 3]
+     *                          =  sum [1 .. 1]    +  sum [2 .. 3]
+     *          Second term:
+     *              sum [4..6]  =  sum [1 .. 6/2]  +  sum [6/2+1 .. 6]
+     *                          =  sum [1 .. 3]    +  sum [4 .. 6]
+     *
+     *
+     *                              1,  2,  3,  4,  5,  6,  7,  8,  9,  10
+     *                              |                   |   |            |
+     *                              1 ............... N/2   N/2+1 ...... N
+     *
+     *                              1,  2,  3,  4,  5,  6,  7,  8,  9,   10
+     *                              |       |   |       |   |   |   |     |
+     *                              1 ... N/2   N/2+1 . N   1 N/2   N/2+1 N
+     *
+     *     1,      2,     3,     4,      5,     6,     7,      8,     9,     10
+     *     |   |   |      |      |   |   |      |      |   |   |      |
+     *     1 N/2   N/2+1  N      1 N/2   N/2+1  N      1 N/2   N/2+1  N
+     *
+     *
      */
-    private int recursiveSum(int n)
+    private static int recursiveSum(int rangeLow, int rangeHigh)
     {
-        int sum;
+        if (rangeHigh > rangeLow)
+        {
+            return rangeHigh + recursiveSum(rangeLow, rangeHigh - 1);
 
-        if (n == 1) {
-            sum = 1;
         }
 
+        if (rangeHigh < rangeLow)
+        {
+            return rangeLow + recursiveSum(rangeHigh, rangeLow - 1);
 
-        return sum;
+        }
+
+        return rangeHigh;
     }
 }
