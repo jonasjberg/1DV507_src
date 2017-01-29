a96aa4ec482baec803cdaae9be1652bf0d55ef5e
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-29 02:51:32 +0100

    Start #4 ..

---
+++ src/js224eh_assign1/PrintJavaMain.java
@@ -0,0 +1,26 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 1: Inheritance, Recursion and Java 1.8
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-01-29.
+
+/*
+ * Exercise 4
+ * ==========
+ * Write a program PrintJavaMain that includes a recursive method
+ * printAllJavaFiles(File directory) that prints all .java files in the
+ * directory and all its sub directories. Both the name of the file and the
+ * size, given as the number of rows, should be printed. All exceptions should
+ * be handled in the program.
+ */
+
+package js224eh_assign1;
+
+
+public class PrintJavaMain
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
