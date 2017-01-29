b9aa31e0ec331711ce53d710527c6bb2d977a59b
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-29 06:05:36 +0100

    Add 'SinMain.java'.

--- src/js224eh_assign1/xchart/SinMain.java
+++ src/js224eh_assign1/xchart/SinMain.java
@@ -1,9 +1,35 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 1: Inheritance, Recursion and Java 1.8
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-01-29.
+
+/*
+ *  Exercise 6
+ *  ----------
+ *  To be submitted:
+ *  Write a program SinMain.java plotting the curve
+ *      y = (1 + x/pi)*cos(x)*cos(40*x) in the range 0 <= x <= 2*pi
+ */
+
 package js224eh_assign1.xchart;
 
 
-/**
- * Created by Jonas Sjöberg (js224eh) on 2017-01-29.
- */
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+
+
 public class SinMain
 {
+    public static void main(String[] args)
+    {
+        // Create and Customize Chart
+        XYChart chart = new XYChartBuilder().width(800).height(600).build();
+        chart.getStyler().setDefaultSeriesRenderStyle(
+                XYSeries.XYSeriesRenderStyle.Line);
+        chart.getStyler().setChartTitleVisible(false);
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSW);
+        chart.getStyler().setMarkerSize(5);
+    }
 }
