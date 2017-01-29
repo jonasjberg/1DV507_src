d723d49c430af8fadc6ff49aab05911df1cd7cec
Jonas Sjöberg <jomeganas@gmail.com>
2017-01-29 13:37:35 +0100

    Update ..

--- src/js224eh_assign1/WarAndPeace.java
+++ src/js224eh_assign1/WarAndPeace.java
@@ -21,8 +21,10 @@
  *  The first lines in your code should look something like:
  *
  *    String text = readText("WarAndPeace.txt"); // My own method
- *    String[] words = text.split(" "); // Simple split, will be cleaned up later on
- *    System.out.println("Initial word count: "+words.length); // We found 577091
+ *    String[] words = text.split(" "); // Simple split, will be cleaned up
+ *    later on
+ *    System.out.println("Initial word count: "+words.length); // We found
+ *    577091
  *
  *    Stream stream = Arrays.stream(words);
  *    ... continue here
@@ -37,20 +39,62 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
 
 
 public class WarAndPeace
 {
-    static final String TEXT_FILE_PATH = "/home/jonas/LNU/1DV507_Datastrukturer/src/1DV507/src/js224eh_assign1/WarAndPeace.txt";
+    static final String TEXT_FILE_PATH =
+            "/home/jonas/LNU/1DV507_Datastrukturer/src/1DV507/src" +
+            "/js224eh_assign1/WarAndPeace.txt";
 
     public static void main(String[] args)
     {
-        String text = readText(TEXT_FILE_PATH);
+        String   text  = readText(TEXT_FILE_PATH);
         String[] words = text.split(" ");
         System.out.println("Initial word count: " + words.length);
 
-        Stream stream = Arrays.stream(words);
+        Stream<String> stream = Arrays.stream(words);
+
+        long c = stream.map(s -> s.trim())
+                       .map(String::toLowerCase)
+                       .map(word -> word.replaceAll("[^a-zA-Z\'-]", ""))
+                       .filter(s -> s.length() > 0)
+                       .distinct()
+                       .count();
+        System.out.println(c);
+
+        // long count = Arrays.stream(text.trim().split(" "))
+        //                    .map(String::toLowerCase)
+        //                    .map(word -> word.replaceAll("[^a-zA-Z\'-]", ""))
+        //                    .collect(joining(" "))
+        //                    .length();
+        // System.out.println(count);
+
+
+        // long c = stream.map(w -> w.toLowerCase()).distinct().count();
+        // System.out.println(c);
+
+
+        // long d = stream.map(word -> word.replaceAll("[^a-zA-Z]", "")
+        // .toLowerCase().trim()).filter(word -> word.length() > 0).count();
+        // System.out.println(d);
+
+/*        stream.map(w -> {
+            return w.replaceAll("[^a-zA-Z\'-]", "");
+        }).count();
+
+        List<String> result = stream.map(String::toLowerCase)
+                                    .map()
+                                    .collect(Collectors.toList());*/
+
+        List<String> list  = Arrays.asList("abc", "def", "ghi");
+        List<String> upped = list.stream().map(String::toUpperCase).collect(
+                Collectors.toList());
+
 
         // TODO: FIX EVERYTHING!!!!!11
 
@@ -60,7 +104,9 @@ public class WarAndPeace
         //long counterStream = stream.filter(w->w.length() > 7).count();
         //System.out.println("Number of words: " + counterStream);
 
-        //long qWords = stream.map(String::toLowerCase) .filter(s->s.startsWith(”q”)).count(); System.out.println(”Words starting with q: ” + qWords);
+        //long qWords = stream.map(String::toLowerCase) .filter(s->s
+        // .startsWith(”q”)).count(); System.out.println(”Words starting with
+        // q: ” + qWords);
     }
 
     /**
