9f21d249988287753a5b8b565ad48828bc669a5b
Jonas Sjöberg <jomeganas@gmail.com>
2017-02-06 18:29:42 +0100

    Start Assignment 2. Add skeleton files.

---
+++ src/js224eh_assign2/queue/LinkedQueue.java
@@ -0,0 +1,63 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 2: Data Structures, JUnit, and JavaFX1
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-02-06.
+
+package js224eh_assign2.queue;
+
+
+import java.util.Iterator;
+
+
+public class LinkedQueue implements Queue
+{
+
+    @Override
+    public int size()
+    {
+        /* TODO: Implement .. */
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        /* TODO: Implement .. */
+        return false;
+    }
+
+    @Override
+    public void enqueue(Object element)
+    {
+        /* TODO: Implement .. */
+    }
+
+    @Override
+    public Object dequeue()
+    {
+        /* TODO: Implement .. */
+        return null;
+    }
+
+    @Override
+    public Object first()
+    {
+        /* TODO: Implement .. */
+        return null;
+    }
+
+    @Override
+    public Object last()
+    {
+        /* TODO: Implement .. */
+        return null;
+    }
+
+    @Override
+    public Iterator<Object> iterator()
+    {
+        /* TODO: Implement .. */
+        return null;
+    }
+}
