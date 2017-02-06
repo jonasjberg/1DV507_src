fb0c01644fb8652b86f78eff004eb775890a8cf2
Jonas Sjöberg <jomeganas@gmail.com>
2017-02-06 18:29:42 +0100

    Start Assignment 2. Add skeleton files.

---
+++ src/js224eh_assign2/queue/QueueMain.java
@@ -0,0 +1,16 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 2: Data Structures, JUnit, and JavaFX1
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-02-06.
+
+package js224eh_assign2.queue;
+
+
+public class QueueMain
+{
+    public static void main(String[] args)
+    {
+        /* TODO: Implement .. */
+    }
+}
