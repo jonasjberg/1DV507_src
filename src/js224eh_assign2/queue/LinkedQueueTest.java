88ced1159666a1b760986383350f63d17f8bb2d5
Jonas Sjöberg <jomeganas@gmail.com>
2017-02-09 14:59:34 +0100

    Add skeleton JUnit test for 'LinkedQueue'.

---
+++ src/js224eh_assign2/queue/LinkedQueueTest.java
@@ -0,0 +1,58 @@
+package js224eh_assign2.queue;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jonas on 2017-02-09.
+ */
+public class LinkedQueueTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+        LinkedQueue queue = new LinkedQueue();
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void Constructor() throws Exception {
+    }
+
+    @org.junit.Test
+    public void size() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void isEmpty() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void enqueue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void dequeue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void first() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void last() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void iterator() throws Exception {
+
+    }
+
+}
