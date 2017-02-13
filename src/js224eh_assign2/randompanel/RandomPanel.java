f3eea0263fed5e5149e85ef83c632692ba1ebb88
Jonas Sjöberg <jomeganas@gmail.com>
2017-02-13 13:55:13 +0100

    Adding scale Text to fit pane in progress.

--- src/js224eh_assign2/randompanel/RandomPanel.java
+++ src/js224eh_assign2/randompanel/RandomPanel.java
@@ -9,8 +9,13 @@
 package js224eh_assign2.randompanel;
 
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.util.Random;
@@ -25,22 +30,34 @@ public class RandomPanel extends BorderPane
     private Text   randomNumber;
     private Random random;
 
+    private ObjectProperty<Font>
+            fontTracking = new SimpleObjectProperty<Font>(Font.getDefault());
+
     public RandomPanel()
     {
         random = new Random();
 
-        button = new Button("New Random");
-        setTop(button);
-
+        /* Setup the text displaying the randomized number. */
         randomNumber = new Text();
         setBottom(randomNumber);
+        setAlignment(randomNumber, Pos.CENTER);
+
+        /* Bind font size to this containers size. */
+        randomNumber.fontProperty().bind(fontTracking);
+        widthProperty().addListener(
+                (observableValue, oldWidth, newWidth) ->
+                        fontTracking.set(Font.font(newWidth.doubleValue() / 4)));
 
 
-        button.setOnAction(actionEvent -> {
+        /* Setup the button that triggers randomization. */
+        button = new Button("New Random");
+        setTop(button);
+        setAlignment(button, Pos.CENTER);
+
+        button.setOnAction((ActionEvent actionEvent) -> {
             int n = random.nextInt((RANDOM_NUMBER_MAX - RANDOM_NUMBER_MIN + 1))
                     + RANDOM_NUMBER_MIN;
             randomNumber.setText(String.valueOf(n));
         });
-
     }
 }
