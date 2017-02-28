a8353dafd79139223979cc2a8461a032c08da2ca
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-01 00:05:15 +0100

    Start working on Exercise 2.

---
+++ src/js224eh_assign3/SortingAlgorithms.java
@@ -0,0 +1,60 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 3: Algorithms, Hashing/BSTs and GUI #2
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-02-28.
+
+// Exercise 2
+// ==========
+// This exercise is about sorting arrays of integer and strings.
+// Create one class SortingAlgorithms containing four static methods:
+//
+//   public int[] insertionSort(int[] in)
+//   public int[] mergeSort(int[] in)                             // VG Exercise
+//   public String[] insertionSort(String[] in, Comparator<String> c)
+//   public String[] mergeSort(String[] in, Comparator<String> c) // VG Exercise
+//
+// All methods return a new sorted array where all elements are sorted in
+// ascending order (lowest first) in the first two integer array methods, as
+// defined by the Comparator in the final two String array methods. The input
+// arrays in should not be changed. The methods should of course use the
+// Insertion Sort and Merge Sort algorithms.
+// 
+// You should also provide a JUnit test class SortTest that tests each method.
+//
+// Notice:
+//   * Our suggestion is that you start to implement (and test) the integer
+//     versions.
+//   * Both algorithms are described in the lecture slides and in the textbook
+//     by Horstmann. You can also find plenty of information on the Internet.
+//   * The textbook (and Internet) also describes how to implement these
+//     algorithms (integer version) in Java. Feel free to take any such
+//     implementation as your starting point. However, in this case you must
+//     clearly state in your assignments submission which implementation you
+//     have used. Provide a web site if you have taken it from the Internet.
+
+package js224eh_assign3;
+
+import java.util.Comparator;
+
+/**
+ * Created by jonas on 2017-03-01.
+ */
+public class SortingAlgorithms {
+
+    public int[] insertionSort(int[] in) {
+        return null;
+    }
+    public int[] mergeSort(int[] in) {
+        return null;
+    }        // VG Exercise
+
+    public String[] insertionSort(String[] in, Comparator<String> c) {
+        return null;
+    }
+
+    public String[] mergeSort(String[] in, Comparator<String> c) {
+        return null;
+    }  // VG Exercise
+
+}
