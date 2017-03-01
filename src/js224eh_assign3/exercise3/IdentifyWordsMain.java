4bcc2de240de7744ee6588e84dcfc031b7d5ae43
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-01 20:30:09 +0100

    Add exercise 3 skeleton.

---
+++ src/js224eh_assign3/exercise3/IdentifyWordsMain.java
@@ -0,0 +1,44 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 3: Algorithms, Hashing/BSTs and GUI #2
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-03-01.
+
+// Exercise 3
+// ==========
+//
+// Write a program IdentyfyWordsMain that reads a text file(like
+// HistoryOfProgramming) and divide the text into a sequence of words (word =
+// sequence of letters). All non-letters (except whitespace) should be removed.
+// Save the result in a new file (words.txt). Example:
+//
+//     Text
+//     ====
+//     Computer programming, History of programming
+//     From Wikipedia, the free encyclopedia (081110)
+//
+//     The earliest known programmable machine (that is a machine whose
+//     behavior can be controlled by changes to a
+//     "program") was Al-Jazari's programmable humanoid robot in 1206.
+//
+//     Sequence of words
+//     =================
+//     Computer programming History of programming
+//     From Wikipedia the free encyclopedia
+//     The earliest known programmable machine that is a machine whose
+//     behavior can be controlled by changes to a
+//     program was Al Jazaris programmable humanoid robot in
+//
+// All exceptions related to file handling shall be handled within the program.
+
+
+package js224eh_assign3.exercise3;
+
+
+public class IdentifyWordsMain
+{
+    public static void main(String[] args)
+    {
+        // TODO: Implement.
+    }
+}
