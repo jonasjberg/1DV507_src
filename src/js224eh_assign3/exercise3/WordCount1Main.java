498fedba549f77f98fdbee300ceda33aac69d768
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-01 21:32:59 +0100

    Add skeleton file 'WordCount1Main.java'.

---
+++ src/js224eh_assign3/exercise3/WordCount1Main.java
@@ -0,0 +1,31 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 3: Algorithms, Hashing/BSTs and GUI #2
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-03-01.
+
+// Exercise 5
+// ==========
+// Create a program WordCount1Main doing the following:
+//
+// For each word in the file word.txt:
+//
+//     1. Create an object of the class Word
+//     2. Add the object to a set of the type java.util.HashSet
+//     3. Add the object to a set of the type java.util.TreeSet
+//
+// Note:
+// 1. The size of the sets should correspond to the number of different words in
+//    the files. (Our tests gave 350 words for the file HistoryOfProgramming)
+// 2. An iteration over the words in the TreeSet should give the words in
+//    alphabetical order.
+// 3. Since our definition of a word is not very precise (similar to the
+//    WarAndPeace exercise in Assignment 2), we do not expect all of you to end
+//    up with exactly 350 words. But it should be rather close.
+
+package js224eh_assign3.exercise3;
+
+
+public class WordCount1Main
+{
+}
