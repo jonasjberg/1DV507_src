f670d65926bb20a24c139e5c0adaf7b9c8cc7692
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-06 22:10:59 +0100

    Update ..

--- src/js224eh_assign3/catch_creature/CreaturePlayfield.java
+++ src/js224eh_assign3/catch_creature/CreaturePlayfield.java
@@ -1,3 +1,22 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 3: Algorithms, Hashing/BSTs and GUI #2
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-03-06.
+
+// Exercise 9
+// ==========
+// Design and implement an application that plays the game Catch-the-Creature.
+// Use an image or icon to represent the creature. Have the creature appear at
+// random locations, then disappear and reappear somewhere else. The goal for
+// the player is to "catch" the creature by pressing the mouse button while the
+// mouse pointer is on the creature image.
+//
+// Create a separate class to represent the creature, and include in it a method
+// that determines if the location of the mouse click corresponds to the current
+// location of the creature. Display a count of the number of times the creature
+// is caught.
+
 package js224eh_assign3.catch_creature;
 
 
@@ -19,6 +38,12 @@ public class CreaturePlayfield extends Pane
 
     private int score;
 
+    /**
+     * Creates a new instance of a CreaturePlayfield.
+     *
+     * @param width The width of the playfield.
+     * @param height The height of the playfield.
+     */
     public CreaturePlayfield(double width, double height)
     {
         PLAYFIELD_WIDTH = width;
