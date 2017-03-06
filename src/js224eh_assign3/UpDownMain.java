d3caedbe7cc210b3d9cd419079f0ec4f6bf8e696
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-06 17:45:37 +0100

    Start work on Exercise 8.

---
+++ src/js224eh_assign3/UpDownMain.java
@@ -0,0 +1,47 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 3: Algorithms, Hashing/BSTs and GUI #2
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-03-06.
+
+// Exercise 8
+// ==========
+// Design and implement a class UpDownPane where the pane contains a pattern of
+// 7x7 smaller panes. The small pane in the middle of the big pane will, once
+// the program is started, contain a small icon (or image). When an arrow key is
+// pressed, the icon will move a step up/down or right/left, to a neighboring
+// small pane. If the icon is moved out of the bigger pane, then the icon will
+// appear on the other side of the bigger pane (i.e. if the icon is moved
+// upwards when in an uppermost small pane, then it will appear on the
+// corresponding small panel in the bottom row). Also write a test program
+// UpDownMain.java, starting a window containing an UpDownPanel
+
+package js224eh_assign3;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class UpDownMain extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.setTitle("1DV507 Assignment 3 -- UpDownPane");
+
+        UpDownPane playField = new UpDownPane(7);
+
+        Scene scene = new Scene(playField, 700, 700);
+        scene.setOnKeyPressed(event -> playField.handleMovementEvent(event));
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
---
+++ src/js224eh_assign3/UpDownPane.java
@@ -0,0 +1,75 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 3: Algorithms, Hashing/BSTs and GUI #2
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-03-06.
+
+// Exercise 8
+// ==========
+// Design and implement a class UpDownPane where the pane contains a pattern of
+// 7x7 smaller panes. The small pane in the middle of the big pane will, once
+// the program is started, contain a small icon (or image). When an arrow key is
+// pressed, the icon will move a step up/down or right/left, to a neighboring
+// small pane. If the icon is moved out of the bigger pane, then the icon will
+// appear on the other side of the bigger pane (i.e. if the icon is moved
+// upwards when in an uppermost small pane, then it will appear on the
+// corresponding small panel in the bottom row). Also write a test program
+// UpDownMain.java, starting a window containing an UpDownPanel
+
+package js224eh_assign3;
+
+
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+
+
+public class UpDownPane extends GridPane
+{
+    private int size;
+    private int xPos;
+    private int yPos;
+
+    public UpDownPane(int size)
+    {
+        this.size = size;
+        xPos = this.size / 2;
+        yPos = this.size / 2;
+    }
+
+    public void handleMovementEvent(KeyEvent event)
+    {
+        switch (event.getCode()) {
+            case LEFT:
+            case H:
+                xPos -= 1;
+                break;
+            case DOWN:
+            case J:
+                yPos -= 1;
+                break;
+            case UP:
+            case K:
+                yPos += 1;
+                break;
+            case RIGHT:
+            case L:
+                xPos += 1;
+                break;
+            default:
+                break;
+        }
+
+        /* Wrap the position if leaving the "area". */
+        if (xPos < 0) {
+            xPos = size - 1;
+        } else if (xPos == size) {
+            xPos = 0;
+        }
+
+        if (yPos < 0) {
+            yPos = size - 1;
+        } else if (yPos == size) {
+            yPos = 0;
+        }
+    }
+}
