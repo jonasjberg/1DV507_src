163cea76ef791db0b5fa6ffae0c65c219df1a195
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-15 20:23:22 +0100

    Update Exercise 1.

---
+++ src/js224eh_assign4/exercise1/LinkedQueue.java
@@ -0,0 +1,14 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 4: Generic Classes and More Algorithms
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-03-15.
+
+/*
+ * Exercise 1
+ * ==========
+ */
+package js224eh_assign4.exercise1;
+
+public class LinkedQueue {
+}
