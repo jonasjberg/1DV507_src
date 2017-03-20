8f3cfe08fe34a02f8e4965d7e2e2bb14b4c92d59
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-20 20:15:19 +0100

    Start Exercise 5.

---
+++ src/js224eh_assign4/binheap/BinaryIntHeap.java
@@ -0,0 +1,62 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 1DV507 -- Programming and Data Structures, VT2017
+// Assignment 4: Generic Classes and More Algorithms
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by Jonas Sjöberg (js224eh) on 2017-03-20.
+
+package js224eh_assign4.binheap;
+
+
+public class BinaryIntHeap
+{
+    /**
+     * Constructs a new empty binary int heap.
+     */
+    public BinaryIntHeap()
+    {
+
+    }
+
+    /**
+     * Inserts a integer "n" to the heap.
+     *
+     * @param n The integer to insert.
+     */
+    public void insert(int n)
+    {
+        // TODO: Implement ..
+    }
+
+    /**
+     * Returns and removes the highest prioritized element.
+     *
+     * @return The element in this heap with the highest priority.
+     */
+    public int pullHighest()
+    {
+        // TODO: Implement ..
+        return 0;
+    }
+
+    /**
+     * Returns the current heap size.
+     *
+     * @return The current size of this heap.
+     */
+    public int size()
+    {
+        // TODO: Implement ..
+        return 0;
+    }
+
+    /**
+     * Returns whether the heap is empty or not.
+     *
+     * @return True if this heap is empty, otherwise False.
+     */
+    public boolean isEmpty()
+    {
+        // TODO: Implement ..
+        return false;
+    }
+}
